3e81cf12059ad037fb3d530cb601a2a9705f0664
iagohfs <iago_henriquefs@hotmail.com>
2018-05-15 09:10:18 +0200

    Add project files.

---
+++ labb_4_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace labb_4_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
