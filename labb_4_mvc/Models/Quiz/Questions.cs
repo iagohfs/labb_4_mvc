54d78a604cfa06ca2355df860f947ef42a7f370e
Unknown <ginokalyun@gmail.com>
2018-06-05 00:56:45 +0200

    Added different questions

--- labb_4_mvc/Models/Quiz/Questions.cs
+++ labb_4_mvc/Models/Quiz/Questions.cs
@@ -13,6 +13,10 @@ namespace labb_4_mvc.Models.Quiz
 
         public string Question { get; set; }
 
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+
         public AnswerChoices CorrectAnswer { get; set; }
 
     }
